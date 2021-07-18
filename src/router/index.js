5f1b0848b716a05cd1e2d9fa858523196a5799fb
Hailemariam Fikadie <se.hailemariam.fikadie@gmail.com>
2021-07-18 20:39:39 +0300

    Designing Components

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Cart from '../views/Cart.vue'
+import Products from '../views/Products.vue'
+import ProductDetail from '../views/ProductDetail.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/product:id',
+    name: 'ProductDetail',
+    component : ProductDetail
+  
+  },
+  {
+    path:'/cart',
+    name:'Cart',
+    component : Cart
+
+  },
+  {
+    path:'/',
+    redirect:'/products'
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1>This is Product Detail </h1>
+</template>
+
+
+<script>
+export default {
+    name:"ProductDetail",
+
+}
+</script>
+
+<style scoped>
+  #page-wrap {
+    margin-top: 16px;
+    padding: 16px;
+    max-width: 600px;
+  }
+
+  #img-wrap {
+    text-align: center;
+  }
+
+  img {
+    width: 400px;
+  }
+
+  #product-details {
+    padding: 16px;
+    position: relative;
+  }
+
+  #add-to-cart {
+    width: 100%;
+  }
+
+  #price {
+    position: absolute;
+    top: 24px;
+    right: 16px;
+  }
+</style>
