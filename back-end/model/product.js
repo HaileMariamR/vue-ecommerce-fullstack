ee0174200712357772c9c8a2a2b063e42ea80af0
Hailemariam Fikadie <se.hailemariam.fikadie@gmail.com>
2021-07-20 14:32:16 +0300

    Design End points

--- back-end/model/product.js
+++ back-end/model/product.js
@@ -5,32 +5,26 @@ const ProductSchema = mongoose.Schema({
         id : {
             type : String,
             required:true,
-            unique : true
         },
         name:{
             type:String , 
             required:true, 
-            unique:true
         }, 
         price:{
             type:String , 
             required:true, 
-            unique:true
         }, 
         description:{
             type:String , 
             required:true, 
-            unique:true
         }, 
         imageUrl:{
             type:String , 
             required:true, 
-            unique:true
         }, 
         averageRating:{
         type:String , 
         required:true, 
-        unique:true
         
     }
         
--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,21 @@
 import express  from 'express'
 import mongoose from 'mongoose'
 import Products from './model/product.js';
-const db = mongoose.connect("mongodb://localhost:12345/v-ecommerce");
+import bodyParser from 'body-parser';
+const db = mongoose.connect("mongodb://localhost:12345/v-ecommerce" , { useNewUrlParser: true } );
+
 
   
-  
-
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 const allRoutes = express.Router()
 allRoutes.route("/Products")
-.get((req, res) =>{
+.get( async (req, res) =>{
 
-    Products.find((err , product)=>{
+ await Products.find((err , product)=>{
         if(err){
             return res.send(err)
         }
@@ -19,7 +23,20 @@ allRoutes.route("/Products")
     })
 
 })
+allRoutes.route('/Products/:ProductId')
+.get(async  (req, res) =>{
+    const ProductId = req.params.ProductId
+  await  Products.findOne({"id" : ProductId} , (err , product)=>{
+    if(err){
+        return res.send(err)
+    }
+    return res.json(product) 
+});
+  
 
+      
+}
+)
 
 
 app.use('/api' , allRoutes)
